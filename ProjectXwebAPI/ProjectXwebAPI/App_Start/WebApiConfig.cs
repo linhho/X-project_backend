146e685e58a25bf9aeb0f9308fcc2565cf3ae90c
kiemhon555 <kiemhon555@gmail.com>
2016-12-20 17:41:45 +0700

    Project X Back-end Complete
    Enable CORS

--- ProjectXwebAPI/ProjectXwebAPI/App_Start/WebApiConfig.cs
+++ ProjectXwebAPI/ProjectXwebAPI/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace ProjectXwebAPI
 {
@@ -11,6 +12,9 @@ namespace ProjectXwebAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
